aa514e151674780540ac3e454e4f4f536980d9f7
VitalijusSeskovas <vitadis5@yahoo.co.uk>
2022-05-11 15:52:15 +0000

    Add Sart Quiz button and Rules box

---
+++ assets/js/quastions.js
@@ -0,0 +1,58 @@
+// Creating an array and passing the number, quastions, options, and answears
+let quastions = [
+    {
+        num: 1,
+        question: "What is a female who practices yoga called?",
+        answer: "Yogini",
+        options: [
+            "Sutra",
+            "Yogi",
+            "Yogini",
+            "Buddhist",
+        ]
+    },
+    {
+        num: 2,
+        question: "What is traditionally chanted at the beginning and end of a yoga session?",
+        answer: "Om",
+        options: [
+            "Hatha",
+            "Om",
+            "Asana",
+            "Samadhi",
+        ]
+    },
+    {
+        num: 3,
+        question: "Where did yoga originate?",
+        answer: "India",
+        options: [
+            "China",
+            "Thailand",
+            "Nepal",
+            "India",
+        ]
+    },
+    {
+        num: 4,
+        question: "What does the word yoga mean?",
+        answer: "Union",
+        options: [
+            "Union",
+            "Peace",
+            "Relaxation",
+            "Soul",
+        ]
+    },
+    {
+        num: 5,
+        question: "In what yoga position do you stand, place a hand on the floor and stretch your arms?",
+        answer: "Triangle",
+        options: [
+            "Cat",
+            "Warrior",
+            "Triangle",
+            "Cobra",
+        ]
+    },
+];
